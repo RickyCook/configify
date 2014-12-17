769d8d52323e0d7788082e348d31629e2f244a75
Ricky Cook <rcook@odecee.com.au>
2014-12-17 21:16:17 +1100

    Refactor main into a class

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os
+
 from jinja2 import (BaseLoader,
                     ChoiceLoader,
                     contextfilter,
@@ -14,53 +16,104 @@ class StraightThroughLoader(BaseLoader):
 @contextfilter
 def inline_tpl_filter(ctx, val, **kwargs):
     params = dict(ctx.get('envs')[0].items() + kwargs.items())
-    return JINJA.get_template(val).render(**params)
+    return ctx.environment.get_template(val).render(**params)
 
-JINJA = Environment(loader=ChoiceLoader((
-    FileSystemLoader('.'),
-    StraightThroughLoader(),
-)))
-JINJA.filters['inline_tpl'] = inline_tpl_filter
+DEFAULT_CONFIG_NAME = 'Configifile'
 
-def get_conf(conf_file):
-    with open(conf_file) as fh:
-        return yaml_load(fh.read().decode())
+class Configify(object):
+    """
+    Configify object that loads all configs, generates params, and then
+    generates the output
+    """
 
-def parse_params_tpls_level(params, level_idx, level):
-    if level is None:
-        level = params
+    def __init__(self, config_path=None):
+        if config_path is None:
+            config_path = os.path.join('.', DEFAULT_CONFIG_NAME)
 
-    if isinstance(level, dict):
-        for idx, val in level.iteritems():
-            level[idx] = parse_params_tpls_level(params, idx, val)
+        if os.path.isdir(config_path):
+            config_path = os.path.join(config_path, DEFAULT_CONFIG_NAME)
 
-    elif isinstance(level, (tuple, list)):
-        for idx, val in enumerate(level):
-            level[idx] = parse_params_tpls_level(params, idx, val)
+        self.config_path = config_path
 
-    else:
-        if isinstance(level_idx, str) and level_idx.startswith('$'):
-            return JINJA.get_template(level).render(**params)
+        self.params = {}
 
-    return level
+    @property
+    def env(self):
+        """
+        Get the Jinja2 environment
+        """
+        if not hasattr(self, '_env'):
+            self._env = Environment(loader=ChoiceLoader((
+                FileSystemLoader('.'),
+                StraightThroughLoader(),
+            )))
+            self._env.filters['inline_tpl'] = inline_tpl_filter
 
-def parse_params_tpls(params):
-    return parse_params_tpls_level(params, None, params)
+        return self._env
 
-def load_params_layer(params_file):
-    yaml_string = JINJA.get_template(params_file).render(**G['params'])
-    G['params'].update(yaml_load(yaml_string))
+    @property
+    def config(self):
+        """
+        Load the config from config_path
+        """
+        if not hasattr(self, '_config'):
+            with open(self.config_path) as fh:
+                self._config = yaml_load(fh.read().decode())
 
-def load_params(params_files):
-    for params_file in params_files:
-        load_params_layer(params_file)
+        return self._config
 
-G = {
-    'params': {}
-}
+    def _parse_params_tpls_level(self, level_idx, level):
+        """
+        Parse an individual level of the params for any templated variables
+        """
+        if isinstance(level, dict):
+            for idx, val in level.iteritems():
+                level[idx] = self._parse_params_tpls_level(idx, val)
 
-G['conf'] = get_conf('conf.yaml')
-load_params(G['conf']['params'])
-G['params'] = parse_params_tpls(G['params'])
+        elif isinstance(level, (tuple, list)):
+            for idx, val in enumerate(level):
+                level[idx] = self._parse_params_tpls_level(idx, val)
 
-print JINJA.get_template(G['conf']['template']).render(**G['params'])
+        else:
+            if isinstance(level_idx, str) and level_idx.startswith('$'):
+                return self.env.get_template(level).render(**self.params)
+
+        return level
+
+    def parse_params_tpls(self):
+        """
+        Parse the full params for any templated variables
+        """
+        return self._parse_params_tpls_level(None, self.params)
+
+    def load_params_layer(self, params_file):
+        """
+        Load an individual layer of params from file on to params
+        """
+        yaml_string = self.env.get_template(params_file).render(**self.params)
+        self.params.update(yaml_load(yaml_string))
+
+    def load_params(self):
+        """
+        Load all params given in the config
+        """
+        params_vals = self.config['params']
+        if isinstance(params_vals, (tuple, list)):
+            for params_file in params_vals:
+                self.load_params_layer(params_file)
+        else:
+            self.load_params_layer(params_vals)
+
+        self.params = self.parse_params_tpls()
+
+    def generate(self):
+        """
+        Return the generated config string
+        """
+        self.load_params()
+
+        return self.env.get_template(
+            self.config['template']
+        ).render(self.params)
+
+print Configify('conf.yaml').generate()
