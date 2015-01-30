cb0aa1fcaf1259b1d6d64e031b94efd1d204b73d
Ricky Cook <rcook@odecee.com.au>
2015-01-30 11:10:19 +1100

    New release

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 
 from setuptools import setup
 
-from pip.req import parse_requirements
-install_reqs = parse_requirements('requirements.txt')
-
-setup(name='Configify',
-      version='0.0.1',
-      description='Templated output, from templated context parameters',
-      author='Ricky Cook',
-      author_email='mail@thatpanda.com',
-      url='https://github.com/RickyCook/configify',
-      py_modules=['configify'],
-      scripts=['configify'],
-      install_requires=[str(pip_req.req) for pip_req in install_reqs],
-     )
+setup(
+    name='Configify',
+    version='0.0.2',
+    description='Templated output, from templated context parameters',
+    author='Ricky Cook',
+    author_email='mail@thatpanda.com',
+    url='https://github.com/RickyCook/configify',
+    py_modules=['configify'],
+    scripts=['configify'],
+    install_requires=[
+        'Jinja2==2.7.3',
+        'MarkupSafe==0.23',
+        'PyYAML==3.11',
+    ],
+ )
